f9db2d044342577161c24e92c7d819baf81312bc
vishnudarrshan <vishnuorp22@gmail.com>
2023-10-04 14:37:57 +0530

    content.js updtd

--- src/Content.js
+++ src/Content.js
@@ -1,22 +1,25 @@
-import React from 'react'
-import ItemLists from './ItemLists'
+import React from 'react';
+import LineItem from './LineItem';
 
-const Content = ({items,handleCheck,handleDelete}) => {
+const Content = ({ items, handleCheck, handleDelete }) => {
   return (
-   
-        <>
-            {(items.length) ? (
-            <ItemLists
-            items={items}
-            handleCheck={handleCheck}
-            handleDelete={handleDelete} />
-) : (<p style={{ marginTop: '2rem' }}>List is Empty</p>)
-}
-             
-                
-        </>
-   
-  )
-}
+    <>
+      {items.length ? (
+        <ul>
+          {items.map((item) => (
+            <LineItem
+              item={item}
+              key={item.id}
+              handleCheck={handleCheck}
+              handleDelete={handleDelete}
+            />
+          ))}
+        </ul>
+      ) : (
+        <p style={{ marginTop: '2rem' }}>List is Empty</p>
+      )}
+    </>
+  );
+};
 
-export default Content
+export default Content;
